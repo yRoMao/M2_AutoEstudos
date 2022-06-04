c5660ba9f636914d00c5619c4b3e3479dcccd296
yRoMao <pedro.dias@sou.inteli.com.br>
2022-06-04 15:37:08 -0300

    exercicio 5

--- 04_AUT_EST_EX_OBRIGATORIOS/Algoritmos Semana 7/ex_5/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Algoritmos Semana 7/ex_5/script.js
@@ -13,18 +13,39 @@ function qtAlunos(){
 function geral(){
     var prova = document.querySelectorAll('.prova');
     var trabalho = document.querySelectorAll('.trabalho')
+    var nome = document.querySelectorAll('.nome')
     var ind = ''
     var geral = 0
     var soma = 0
+    var soma_p = 0
+    var soma_t = 0
+    var maior_nota_p = 0
+    var menor_nota_p = 1000
+    var maior_nota_t = 0
+    var menor_nota_t = 1000
     for(var i = 0; i<prova.length; i++){
         var media = ((parseInt(prova[i].value) * 2) + (parseInt(trabalho[i].value) * 3)) / 5
-        ind += 'aluno ' + (i + 1) + ': ' + media + ' '
+        ind += '<h1>Aluno ' + nome[i].value + ': ' + media + '</h1>'
         soma += media
+        soma_p += parseInt(prova[i].value)
+        soma_t += parseInt(trabalho[i].value)
+        if (parseInt(prova[i].value) > maior_nota_p){
+            maior_nota_p = parseInt(prova[i].value)
+        }
+        if ((parseInt(prova[i].value) < menor_nota_p)){
+            menor_nota_p = parseInt(prova[i].value)
+        }
+        if (parseInt(trabalho[i].value) > maior_nota_t){
+            maior_nota_t = parseInt(trabalho[i].value)
+        }
+        if ((parseInt(trabalho[i].value) < menor_nota_t)){
+            menor_nota_t = parseInt(trabalho[i].value)
+        }
         }
     if (i == prova.length){
         geral = soma / prova.length
-        alert(ind + 'Média geral: ' + geral)
+        var media_p = soma_p / prova.length
+        var media_t = soma_t / trabalho.length
+        document.getElementById('result').innerHTML = ind + '<h1>Media geral: ' + geral + '</h1>' + '<h1>Media provas: ' + media_p + '</h1>' + '<h1>Media Trabalhos: ' + media_t + '</h1>' + '<h1>Menor e maior notas prova: ' + menor_nota_p + '//' + maior_nota_p + '</h1>' + '<h1>Menor e maior notas trabalho: ' + menor_nota_t + '//' + maior_nota_t + '</h1>'
     }
 }
-
-//function 
