0a70f1c7bc1609efb65d7f8e423457d88a117459
yRoMao <pedro.dias@sou.inteli.com.br>
2022-06-13 19:45:22 -0300

    etapa 4

---
+++ 03_AUT_EST_ENTREGA/Etapa 4/script.js
@@ -0,0 +1,13 @@
+function AddLine()
+{
+    var click = false
+    if (!click) {
+        const node = document.createElement("h1");
+        const textnode = document.createTextNode("Segredinho, shiuuu!!");
+        node.appendChild(textnode);
+        $("#info").prepend(node);
+        var click = true
+    } else {
+        document.getElementById('info').removeChild(document.getElementById('info').getElementsByTagName('h1')[0]);
+    }
+}
